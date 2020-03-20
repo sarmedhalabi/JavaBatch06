42b1c39bf73becd8d956129bd7d75834968f582f
sarmedhalabi <sarmedhalabi@gmail.com>
2020-03-20 01:54:43 -0400

    Q 6 and Q7 solved

--- src/com/syntax/class12/CharAtExample.java
+++ src/com/syntax/class12/CharAtExample.java
@@ -6,12 +6,12 @@ public class CharAtExample {
 		// TODO Auto-generated method stub
 String str="Good Morning Students!";
         
-        System.out.println("----- charAt() FUNCTION -----");
-        char letter1=str.charAt(0);
-        System.out.println(letter1);
-        
-        char letter5=str.charAt(4);
-        System.out.println("Letter at index 4 is "+letter5+".");
+//        System.out.println("----- charAt() FUNCTION -----");
+//        char letter1=str.charAt(0);
+//        System.out.println(letter1);
+//        
+//        char letter5=str.charAt(4);
+//        System.out.println("Letter at index 4 is "+letter5+".");
         
 //      char someLetter=str.charAt(55);// RunTime Exception --> StringIndexOutOfBoundsException
 //      System.out.println(someLetter);
@@ -28,35 +28,37 @@ String str="Good Morning Students!";
         System.out.println("----- indexOf() FUNCTION -----");
         
         String name="Syntax Technologies";
-        int index=name.indexOf("y");
-        System.out.println(index);  
-        
-        index=name.indexOf(" ");
-        System.out.println(index);
-        
-        index=name.indexOf("Syntax");
-        System.out.println(index);
-        
-        index=name.indexOf("!");
-        System.out.println("Index of not existing character="+index);
-        
-        index=name.indexOf("Technologies");
-        System.out.println("Index of substring Technologies="+index);
-        
-        index=name.indexOf("o");
-        System.out.println("Index of first o="+index);
-        
-        index=name.lastIndexOf("o");
-        System.out.println("Index of second o="+index);
-        
-        String name1=" i love my school";
-        int num=name1.indexOf("love");
-        char letter2=name1.charAt(1);
-        System.out.println(letter2);
-        
-        
-        System.out.println(num);
-        
+        int names= name.length();
+        System.out.println(name.charAt(names));
+//        int index=name.indexOf("y");
+//        System.out.println(index);  
+//        
+//        index=name.indexOf(" ");
+//        System.out.println(index);
+//        
+//        index=name.indexOf("Syntax");
+//        System.out.println(index);
+//        
+//        index=name.indexOf("!");
+//        System.out.println("Index of not existing character="+index);
+//        
+//        index=name.indexOf("Technologies");
+//        System.out.println("Index of substring Technologies="+index);
+//        
+//        index=name.indexOf("o");
+//        System.out.println("Index of first o="+index);
+//        
+//        index=name.lastIndexOf("o");
+//        System.out.println("Index of second o="+index);
+//        
+//        String name1=" i love my school";
+//        int num=name1.indexOf("love");
+//        char letter2=name1.charAt(1);
+//        System.out.println(letter2);
+//        
+//        
+//        System.out.println(num);
+//        
     }
 
 	}
