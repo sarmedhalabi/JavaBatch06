c53c9aeea1c68fc3fbcce205eacfb314aa257ac8
sarmedhalabi <sarmedhalabi@gmail.com>
2020-03-21 15:00:39 -0400

    updated

---
+++ src/com/syntax/class14/MethodExmple.java
@@ -0,0 +1,16 @@
+package com.syntax.class14;
+
+public class MethodExmple {
+	void greet() {System.out.println("Hello world");}
+	void greet1(int letter) {System.out.println("hello"+ letter);}
+	public static void main(String[] args) {
+		MethodExmple object1=new MethodExmple();
+		object1.greet();
+		object1.greet1(234);
+		
+		
+		
+		
+	}
+
+}
