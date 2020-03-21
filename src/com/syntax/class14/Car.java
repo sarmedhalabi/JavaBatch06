a1cd1a3efb1e4dc67aef0a41498489e53e426204
sarmedhalabi <sarmedhalabi@gmail.com>
2020-03-21 13:17:34 -0400

    class 14 added

---
+++ src/com/syntax/class14/Car.java
@@ -0,0 +1,46 @@
+package com.syntax.class14;
+
+public class Car {
+	String name;
+	String color;
+	int age;
+	String breed;
+	
+	
+	public static void main(String [] args) {
+	Car Dog	=new Car();
+	Dog .name="lucy";
+	Dog.age=12;
+	Dog.breed="golden retiver";
+	Dog.action();
+	
+	
+		
+	System.out.println(Dog.breed);	
+	Dog.action();
+	
+			
+	}
+	void action() {System.out.println(name+ " is nice ");}
+	
+	}
+	
+
+	
+
+//	String make;
+//	String color;
+//	int speed;
+//	String size;
+//
+//
+//public static void main(String [] args) {
+//	Car sarmed=new Car();
+//	sarmed.make="bmw";
+//	sarmed.speed=300;
+//	sarmed.size="big";
+//	System.out.println(sarmed.make);
+//	
+//}
+//	
+//}
